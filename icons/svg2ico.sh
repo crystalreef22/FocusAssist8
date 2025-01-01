5468eb43bdab8172486a449ff4b2feaae7c13f70
crystalreef22 <119718492+crystalreef22@users.noreply.github.com>
2025-01-01 13:38:33 -0500

    made .ico icon

---
+++ icons/svg2ico.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# from https://graphicdesign.stackexchange.com/questions/77359/how-to-convert-a-square-svg-to-all-size-ico
+magick focus\ assist\ icon\ exported\ texttopath.svg -define icon:auto-resize focusassisticon.ico
